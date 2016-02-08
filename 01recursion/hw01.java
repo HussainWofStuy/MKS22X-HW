557879a3a36b6f4798590c3d493dc7a8a0b20f52
Hussain Waris <hwaris@stuy.edu>
2016-02-08 11:26:07 -0500

    Organised function/classes that way they need to be

--- 01recursion/hw01.java
+++ 01recursion/hw01.java
@@ -1,10 +1,4 @@
 public interface hw01{
-    double guess = 1; 
-    public String name(){return "Waris,Hussain";}
-    public double sqrt(double n){
-	if(n < 0){throw new IllegalArgumentException("Input must be greater than or equal to O");}
-	if(Math.abs(((Math.pow(guess,2)-n)/n)*100) < 0.000001){return guess;}
-	guess = (n / guess + guess) / 2;
-	return sqrt(n);
-    }
+    public String name();
+    public double sqrt(double n);
 }
