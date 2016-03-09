a0934e57d42318ac4805bc4dbde76179f5f692ac
Hussain Waris <hwaris@stuy.edu>
2016-03-08 23:16:52 -0500

    Done

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -2,37 +2,55 @@ import java.util.*;
 public class Quick{
     public Quick(){}
 
-    public static int partition(int[] data, int left, int right){
-	Random rand = new Random();
-	int c = rand.nextInt(right-left+1) + left;
-	int d = data[c];
-	System.out.println(d);
-	data[c] = data[right];
-	data[right] = d;
-	int tempLeft = left;
-	int tempRight = right - 1;
-	while(tempLeft != tempRight){
-	    if(data[tempLeft] <= d){tempLeft++;}
-	    if(data[tempLeft] > d){
-		int s1 = data[tempLeft];
-		data[tempLeft] = data[tempRight];
-		data[tempRight] = s1;
-		tempRight--;
+    public String name(){return "6,Waris,Hussain";}
+    
+    public static int quickselect(int[] data, int k){
+	k = k-1;
+	k = data.length - k;
+	return quickSelect(data, k, 0, data.length-1);
+    }
+    
+    public static int quickSelect(int[] data, int k, int left, int right){
+	if(left <= right){
+	    if(right==left){return data[left];}
+	    int f = partition(data, left, right);
+	    if(f==k){return data[k];}
+	    if(f > k){
+		return quickSelect(data, k, left, f-1);
+	    }
+	    if(f < k){
+		return quickSelect(data, k, f+1, right);
 	    }
 	}
-	data[right] = data[tempRight+1];
-	data[tempRight+1] = d;
-	if(data[tempRight] > d){
-	    data[tempRight+1] = data[tempRight];
-	    data[tempRight] = d;
-	}
-	return tempRight+1;
+	return 0;
     }
 
-    public static void main(String[] args){
-	int[] g = new int[]{4,6,13,99,45,113,112,9,6,5,-88,1000,345,66,44};
-	System.out.println(Arrays.toString(g));
-	System.out.println(new Quick().partition(g, 0, g.length-1));
-	System.out.println(Arrays.toString(g));
+    private void swap(int[] data, int x, int y){
+	int nX = data[x];
+	data[x] = data[y];
+	data[y] = nX;
     }
+    
+    public static int partition(int[] data, int left, int right){
+	Quick q = new Quick();
+	int f = left + new Random().nextInt(right-left+1);
+	q.swap(data, right, f);
+	for (int i = left; i < right; i++) {
+	    if (data[i] > data[right]) {
+		q.swap(data, i, left);
+		left++;
+	    }
+	}
+	q.swap(data, left, right);
+	return left;
+    }
+
+    /*
+    public static void main(String[] args){
+	int[] g = new int[]{1, 2, 3, 4, 5,6};
+	
+	System.out.println(new Quick().quickselect(g, 2));
+	
+    }
+    */
 }
