580f389b2dae31866a77d42696677b6af1182f9b
Hussain Waris <hwaris@stuy.edu>
2016-03-25 16:40:48 -0400

    Done

--- 08linkedList/MyLinkedList.java
+++ 08linkedList/MyLinkedList.java
@@ -1,5 +1,28 @@
 import java.util.*;
-public class MyLinkedList<T>{
+//anonymous class - class you only declare once because you only need it once
+public class MyLinkedList<T> implements Iterable<T>{
+    public class MyLinkedListIteratorThing implements Iterator<T>{
+	private LNode next;
+	public MyLinkedListIteratorThing(){
+	    next = start;
+	}
+	public boolean  hasNext(){
+	    return next != null;
+	}
+	public T next(){
+	    if(!hasNext()){throw new NoSuchElementException();}
+	    T temp = next.getValue();
+	    next = next.getNext();
+	    return temp;
+	}
+	
+	public void remove(){throw new UnsupportedOperationException();}
+    }
+
+    public Iterator<T> iterator(){
+	return new MyLinkedListIteratorThing();
+    }
+       		
     private class LNode{
 	private T data;
 	private LNode next;
@@ -14,7 +37,9 @@ public class MyLinkedList<T>{
     private LNode end;
     private int size;
 
-    public  boolean add(T value){
+    public int size(){return size;}
+    
+    public boolean add(T value){
 	if(start == null){
 	    start = new LNode(value);
 	    end = start;
@@ -42,7 +67,14 @@ public class MyLinkedList<T>{
     }
     */
 
-    public boolean add(T value, int index){
+    public boolean add(int index, T value){
+	if(index < 0 || index > size){throw new IndexOutOfBoundsException();}
+	if(index == size){
+	    end.setNext(new LNode(value));
+	    end = end.getNext();
+	    size++;
+	    return true;
+	}
 	if(index == 0){
 	    LNode p = start;
 	    start = new LNode(value);
@@ -84,9 +116,12 @@ public class MyLinkedList<T>{
     }
     
     public T remove(int i){
+	if(i < 0 || i >= size){throw new IndexOutOfBoundsException();}
+	
 	if(i==0){
 	    T rV = start.getValue();
 	    start = start.getNext();
+	    size--;
 	    return rV;
 	}else{
 	    LNode p = start;
@@ -95,6 +130,7 @@ public class MyLinkedList<T>{
 	    }
 	    T rV = p.getNext().getValue();
 	    p.setNext(p.getNext().getNext());
+	    if(i == size()-1){end = p;}
 	    size--;
 	    return rV;
 	}
@@ -108,7 +144,8 @@ public class MyLinkedList<T>{
 	return p.getValue();
     }
 
-    public T set(int i, int v){
+    public T set(int i, T v){
+	if(i < 0 || i >= size){throw new IndexOutOfBoundsException();}
 	LNode p = start;
 	for(int k = 0; k < i; k++){
 	    p = p.getNext();
@@ -124,41 +161,17 @@ public class MyLinkedList<T>{
 	while(p!=null){
 	    s += p.getValue();
 	    if(p.getNext()!=null){
-		s += ",";
+		s += ", ";
 	    }
 	    p = p.getNext();
 	}
 	return s + "]";
     } 
 
-    /*
     public static void main(String[]args){
-	MyLinkedList<String> m = new MyLinkedList<String>();
+	MyLinkedList<Integer> m = new MyLinkedList<Integer>();
+	for(int x = 0, x < 21; x+=2){
+    }
 
-	m.add(new String("Hello?"));
-	System.out.println(m);
-
-	MyLinkedList< m = new MyLinkedList();
-
-	ArrayList<Integer> n = new ArrayList<Integer>();
-       
-	long start,end;
-		
-	start = System.currentTimeMillis();
-	//code to test here on ArrayList n
-	for(int i = 0; i < 10000000; i++){
-	    n.add(i);
-	}
-	end = System.currentTimeMillis();
-	System.out.println("Time: " + (end-start)/1000.0 + " seconds for ArrayList");
-	
-	start = System.currentTimeMillis();
-	for(int i = 0; i < 10000000; i++){
-	    m.add(i);
-	}
-	end = System.currentTimeMillis();
-	System.out.println("Time: " + (end-start)/1000.0 + " seconds for MyLinkedList");	
-       
-    }    
-    */
+    
 }
